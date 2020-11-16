8e8ec2eab5f24494fbbf5a2115d834f9157d2425
Samet Sahin <a.sahin.ual@gmail.com>
2020-11-17 02:16:01 +0300

    design the game board along with icons for players and golds

---
+++ src/com/company/Board.java
@@ -0,0 +1,60 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+
+public class Board {
+    private final int numOfRows;
+    private final int numOfColumns;
+
+    private JPanel boardPanel;
+    private final ArrayList<JLabel> cells = new ArrayList<>();
+
+    public Board(int numOfRows, int numOfColumns) {
+        this.numOfRows = numOfRows;
+        this.numOfColumns = numOfColumns;
+        initializeGUI();
+    }
+
+    public void initializeGUI() {
+        boardPanel = new JPanel(new GridLayout(numOfRows, numOfColumns));
+
+        ImageIcon aLetterIcon = new ImageIcon(getClass().getResource("assets/a_letter_icon.png"));
+        ImageIcon bLetterIcon = new ImageIcon(getClass().getResource("assets/b_letter_icon.png"));
+        ImageIcon cLetterIcon = new ImageIcon(getClass().getResource("assets/c_letter_icon.png"));
+        ImageIcon dLetterIcon = new ImageIcon(getClass().getResource("assets/d_letter_icon.png"));
+        ImageIcon spinningGoldGIF = new ImageIcon(getClass().getResource("assets/gold_icon.png"));
+
+        for (int i = 0; i < numOfRows; i++) {
+            for (int j = 0; j < numOfColumns; j++) {
+                JLabel label = new JLabel();
+                if (i == 0 && j == 0) {
+                    // First cell
+                    label.setIcon(aLetterIcon);
+                } else if (i == 0 && j == numOfColumns - 1) {
+                    // Last cell of the first row
+                    label.setIcon(bLetterIcon);
+                } else if (i == numOfRows - 1 && j == 0) {
+                    // First cell of the last row
+                    label.setIcon(cLetterIcon);
+                } else if (i == numOfRows - 1 && j == numOfColumns - 1) {
+                    // Last cell
+                    label.setIcon(dLetterIcon);
+                } else {
+                    // A cell that is not a side.
+                    label.setIcon(spinningGoldGIF);
+                }
+                label.setHorizontalAlignment(SwingConstants.CENTER);
+                label.setVerticalAlignment(SwingConstants.CENTER);
+                label.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+                cells.add(label);
+                boardPanel.add(label);
+            }
+        }
+    }
+
+    public JPanel getBoardPanel() {
+        return boardPanel;
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,21 @@
 package com.company;
 
-public class Main {
+import javax.swing.*;
 
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello from Jetbrains IntelliJ Idea");
+        Runnable runnable = () -> {
+            JFrame frame = new JFrame("Gold Hunt");
+
+            Board board = new Board(20, 15);
+            frame.add(board.getBoardPanel());
+
+            frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+            frame.pack();
+            frame.setSize(1400, 1000);
+            frame.setLocationRelativeTo(null);
+            frame.setVisible(true);
+        };
+        SwingUtilities.invokeLater(runnable);
     }
 }
